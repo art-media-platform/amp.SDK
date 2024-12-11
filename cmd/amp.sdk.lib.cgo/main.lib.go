aa7d64348756a96fd9f8e05e6140983c5d399588
Drew O'Meara <drew@plan-systems.org>
2024-12-10 18:54:40 -0600

    updated _TODO.txt

--- cmd/amp.sdk.lib.cgo/main.lib.go
+++
@@ -1,3 +0,0 @@
-package main
-
-// TODO -- export C wrappers for package tags and other items in amp.sdk
--- cmd/amp.tag.tool.go/main.go
+++
@@ -1,3 +0,0 @@
-package main
-
-// TODO: cli tool for Amp's 24 byte tagging system -- "spec-tag"?
