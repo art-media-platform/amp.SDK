452cffc56e34949ee9349e521ad13906db49cc47
Drew O'Meara <drew@plan-systems.org>
2026-04-03 16:54:33 -0500

    LaunchURL now implemented organically

--- stdlib/platform/utils.go
+++ stdlib/platform/utils.go
@@ -1,12 +1,27 @@
 package platform
 
 import (
-	"github.com/pkg/browser"
+	"io"
+	"os"
+	"os/exec"
 )
+
+// Stdout is the io.Writer to which executed commands write standard output.
+var Stdout io.Writer = os.Stdout
+
+// Stderr is the io.Writer to which executed commands write standard error.
+var Stderr io.Writer = os.Stderr
 
 // LaunchURL() pushes an OS-level event to open the given URL using the user's default / primary browser.
 //
 // For future-proofing, use this instead of browser.OpenURL().
 func LaunchURL(url string) error {
-	return browser.OpenURL(url)
+	return runCmd("open", url)
+}
+
+func runCmd(prog string, args ...string) error {
+	cmd := exec.Command(prog, args...)
+	cmd.Stdout = Stdout
+	cmd.Stderr = Stderr
+	return cmd.Run()
 }
